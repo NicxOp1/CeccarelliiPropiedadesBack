81b74e272f4acd01e590d735c5b07f2585205b40
RamiroRojas02 <ramirojasarg@gmail.com>
2023-01-04 19:14:38 -0300

    filtro typeValue

--- controllers/property.js
+++ controllers/property.js
@@ -3,9 +3,21 @@ const Property = require('../models/Property')
 const controller = {
     read: async (req,res)=>{
         let query = {}
+        let min = {}
+if (req.query.typeValue) {
+    query = {
+        ...query,
+        typeValue: req.query.typeValue
+    }
+}
+if (req.query.min) {
+    min = {
+        value: req.query.min
+    }
+}
+
         try {
-            let properties = await Property.find()
-            console.log(properties);
+            let properties = await Property.find(query).gte(min)
             if(properties){
                 res.status(200).json({
                     properties,
